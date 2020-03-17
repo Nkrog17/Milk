5334c06d15bf94eb0b5ddaa2b39250a4a6651808
Mads Strømberg <mpet17@student.aau.dk>
2020-03-17 15:32:35 +0100

    Har addet en lyd på et sted på gulvet som NPC'en reagerer på og resetter efter 10sek

---
+++ Game prototype/Assets/Scripts/InvestigatingScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class InvestigatingScript : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    public float timer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If there is a noise. Move. The input is position on the 2d plane of the screen.
+        if (NoiseScript.noiseOn == true) {
+            Ray ray = cam.ScreenPointToRay(new Vector3(533, 409, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+            timer = timer + Time.deltaTime;
+
+            //After ten seconds NPC will return to position.
+            if (timer > 10) {
+                timer = 0;
+                NoiseScript.noiseOn = false;
+                Debug.Log(NoiseScript.noiseOn);
+                
+            }
+        }
+
+        if (NoiseScript.noiseOn == false) {
+            Ray ray = cam.ScreenPointToRay(new Vector3(400, 100, 0));
+            RaycastHit hit;
+            Debug.Log("OMW Back");
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
---
+++ Game prototype/Assets/Scripts/NoiseScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseScript : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    public static Vector3 position;
+    public static bool noiseOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider other)
+    {
+        audioSource.Play();
+        Debug.Log("Im in the trigger");
+        Debug.Log(transform.position);
+        position = transform.position;
+        noiseOn = true;
+        Debug.Log(noiseOn);
+        Debug.Log(Input.mousePosition);
+    }
+}
