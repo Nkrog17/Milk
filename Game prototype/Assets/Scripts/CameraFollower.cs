6dd12346cb4d5066caa752f9a64ed0437ddb00c2
Niels <biuerboris@gmail.com>
2020-03-29 16:18:23 +0200

    Tilføjede kamera til spiller

---
+++ Game prototype/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+   	public GameObject player;
+   	public float yOffset;
+   	Vector3 offset;
+
+   	void Start(){
+   		 offset = new Vector3(0,yOffset,0);
+   	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
