420b2c843e669d14fe13a5ea8f7a33fa04efb6d8
Niklas Lee Skjold Hansen <Skjold20@gmail.com>
2020-04-15 12:51:17 +0200

    fridge script

---
+++ Game prototype/Assets/DoorAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnim : MonoBehaviour
+{
+    private Animator anim;
+    
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("isOpen", true);
+          
+        }
+  
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("IsOpen", false);
+    }
+}
